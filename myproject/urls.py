e0192ca63fc82ec7867423e02512903e3ee0f0d8
msantoshkumarmeher <msantoshkumarmeher@gmail.com>
2026-03-28 23:48:49 +0530

    Completed Project-added screenshots

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/onlinecourse/', permanent=False)),
     path('onlinecourse/', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
